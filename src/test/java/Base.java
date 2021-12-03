4567f779de297ba89c3722efb67fd9f0e96a0732
fzafeiriou <71286243+fzafeiriou@users.noreply.github.com>
2021-12-03 22:02:12 +0100

    Added unit test

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,13 +1,30 @@
-  package net.codejava;
+package net.codejava;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class TestFormApplication {
-   
-   User testUser = new User();
-
-   protected void run() {
+public class SomeTest extends TestFormApplication {
+   @Test public void test1() {
+      run();
    }
 
+   @Test public void test2() {
+      run();
+   }
+  
+  
+   // test the setUserName
+   @Test public void testSetName() {
+      testUser.setName("Norma");
+      String thisTestName = testUser.getName();
+      assertEquals("Norma", thisTestName);
+   }
+
+   // test the setGender
+   @Test public void testSetGender() {
+      testUser.setGender("Female");
+      String thisTestGender = testUser.getGender();
+      assertEquals("Female", thisTestGender);
+   }
+  
 }
