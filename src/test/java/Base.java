9d00c9ab58d42dd253548be6c3aa28b2d94f06f1
fzafeiriou <71286243+fzafeiriou@users.noreply.github.com>
2021-12-03 22:06:46 +0100

    Update Base.java

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,30 +1,13 @@
-package net.codejava;
+  package net.codejava;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class SomeTest extends TestFormApplication {
-   @Test public void test1() {
-      run();
+class TestFormApplication {
+   
+   User testUser = new User();
+
+   protected void run() {
    }
 
-   @Test public void test2() {
-      run();
-   }
-  
-  
-   // test the setUserName
-   @Test public void testSetName() {
-      testUser.setName("Norma");
-      String thisTestName = testUser.getName();
-      assertEquals("Norma", thisTestName);
-   }
-
-   // test the setGender
-   @Test public void testSetGender() {
-      testUser.setGender("Female");
-      String thisTestGender = testUser.getGender();
-      assertEquals("Female", thisTestGender);
-   }
-  
 }
