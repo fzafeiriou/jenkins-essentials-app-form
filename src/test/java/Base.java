d336e663f4566aa33ab8d01ade90182245d24a43
fzafeiriou <71286243+fzafeiriou@users.noreply.github.com>
2021-12-03 22:19:30 +0100

    Update Base.java

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,8 +1,5 @@
 package net.codejava;
 import org.junit.Test;
 import static org.junit.Assert.*;
-class TestFormApplication {
-   User testUser = new User();
-   protected void run() {
-   }
-}
+class TestFormApplication {User testUser = new User();
+   protected void run() {}}
